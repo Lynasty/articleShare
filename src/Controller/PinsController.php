7e07b7931718f197c714e78d171ada51631f7922
Darwin <darwin_95@msn.com>
2021-11-15 16:05:11 +0100

    Added Registration form and email verifier

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Pin;
 use App\Form\PinType;
 use App\Repository\PinRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,13 +24,14 @@ class PinsController extends AbstractController
     }
 
     #[Route('/pins/create', name: 'app_pins_create', methods: ['GET','POST'])]
-    public function create(Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepo): Response
     {
         $pin = new Pin;
         $form = $this->createForm(PinType::class, $pin);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
+            $pin->setUser($this->getUser());
             $em->persist($pin);
             $em->flush();
 
