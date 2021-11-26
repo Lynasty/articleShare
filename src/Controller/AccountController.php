14a1c4e0c636c291e454e61731e89e0b21b42494
Darwin <darwin_95@msn.com>
2021-11-26 20:33:11 +0100

    Add Change Password to Account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordFormType;
 use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -32,6 +34,29 @@ class AccountController extends AbstractController
             return $this->redirectToRoute('app_account');
         }
         return $this->render('account/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/account/change-password', name: 'app_account_change_password')]
+    public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordFormType::class, null, [
+            'current_password_is_required' => true
+        ]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $form['plainPassword']->getData())
+            );
+            $em->flush();
+
+            $this->addFlash('success', 'Password updated');
+
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/change_password.html.twig', [
             'form' => $form->createView()
         ]);
     }
--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -14,6 +15,19 @@ class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        if ($options['current_password_is_required']) {
+            $builder
+                ->add('currentPassword', PasswordType::class, [
+                    'label' => 'Current Password',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter your current password',
+                        ]),
+                        new UserPassword,
+                    ],
+                ]);
+        }
+
         $builder
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
@@ -30,7 +44,7 @@ class ChangePasswordFormType extends AbstractType
                             'max' => 4096,
                         ]),
                     ],
-                    'label' => 'New password',
+                    'label' => 'New Password',
                 ],
                 'second_options' => [
                     'attr' => ['autocomplete' => 'new-password'],
@@ -40,12 +54,13 @@ class ChangePasswordFormType extends AbstractType
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'current_password_is_required' => false
+        ]);
     }
 }
